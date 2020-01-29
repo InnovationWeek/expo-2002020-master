078d51d127441de6d736468af9eb4994459606e7
Elise Boes <elise.boes@student.ehb.be>
2020-01-29 16:02:28 +0100

    Add functionality

--- js/sketch.js
+++ js/sketch.js
@@ -1,3 +1,5 @@
+let selectedYear, selectedContinent, selectedDisease, selectedAge;
+
 $(function () {
   $('#slider').slider({
     range: "max",
@@ -5,38 +7,52 @@ $(function () {
     max: 7,
     value: 2,
     slide: function (event, ui) {
-      console.log(ui.value);
-      if (ui.value == 0) {
-        $('#amount').empty().append(`<p>Selected age: ${ui.value}</p>`)
-      } else if (ui.value == 7) {
-        $('#amount').empty().append(`<p>Selected age: 70+</p>`)
-      } else {
-        $('#amount').empty().append(`<p>Selected age: ${ui.value}0</p>`)
+      if(ui.value == 0){
+        selectedAge = "0-28 days";
+      }else if(ui.value == 1){
+        selectedAge = "1-59 months";
+      }else if(ui.value == 2){
+        selectedAge = "5-14 years";
+      }else if(ui.value == 3){
+        selectedAge = "15-29 years";
+      }else if(ui.value == 4){
+        selectedAge = "30-49 years";
+      }else if(ui.value == 5){
+        selectedAge = "50-59 years";
+      }else if(ui.value == 6){
+        selectedAge = "60-69 years";
+      }else{
+        selectedAge = "70+ years";  
       }
+
+      $('#amount').empty().append(`<p>Selected age: ${selectedAge}</p>`)
+      fileChecker(selectedContinent, selectedYear);
     }
   });
   $("#amount").val($("#slider-range-max").slider("value"));
   
 
 $('#opt-year li').click(function () {
-  console.log($(this).text())
-  if ($(this).text() == "2016") {
-    loadJSON('./data/json/GHE2016_DALY.json', getSixtData);
-  }else if ($(this).text() == "2015"){
-    loadJSON('./data/json/GHE2015_DALY.json', getFiftData);
-  }else if($(this).text() == "2010"){
-    loadJSON('.data/json/GHE2010_DALY.json', getTenthData);
-  }
+  selectedYear = $(this).text();
+  fileChecker(selectedContinent, selectedYear);
 })
 
-$('')
+$('#opt-disease li').click(function(){
+  selectedDisease = $(this).text();
+  fileChecker(selectedContinent, selectedYear);
+});
+
+$('#opt-region li').click(function(){
+  selectedContinent = $(this).text();
+  fileChecker(selectedContinent, selectedYear);
+});
+
 })
 
 function setup() {
   createCanvas(1920, 1080);
   colorMode(HSB, 360, 100, 100, 100);
   background(0);
-  translate(700, 600);
   //drawFractal(0, 0, 300);
   //drawBubbles();
   // var len = 100;
@@ -49,6 +65,32 @@ function setup() {
 
 function draw() {
 
+}
+
+function fileChecker(continent, year){
+  if(continent == 'Africa'){
+    loadJSON('./data/json/AFR'+year+'.json', getData);
+  }else if (continent == 'Western Pacific'){
+    loadJSON('./data/json/WPR'+year+'.json', getData);
+  }else if(continent == 'America'){
+    loadJSON('./data/json/AMR'+year+'.json', getData);
+  }else if (continent == 'Europe'){
+    loadJSON('./data/json/EUR'+year+'.json', getData);
+  }else if(continent == 'Southeast Asia'){
+    loadJSON('./data/json/SEAR'+year+'.json', getData);
+  }else{
+    loadJSON('./data/json/EMR'+year+'.json', getData);
+  }
+  console.log(selectedContinent)
+  console.log(selectedAge)
+  console.log(selectedYear)
+  console.log(selectedDisease)
+
+  }
+
+function getData(data){
+      console.log("Male:" + data.diseases[selectedDisease].ages.male[selectedAge]);
+      console.log("Female:" + data.diseases[selectedDisease].ages.female[selectedAge]);
 }
 
 function getSixtData(sixtData) {
